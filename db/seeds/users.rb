693e89a124658ffdbac4a779dcf7032e951c75ed
Lachlan Douglas <lachlan@engines.org>
2018-08-06 12:19:11 +1000

    debug

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -1,19 +1,19 @@
 User.create!([
-  {
-    email: "lach@engines.org",
-    password: "password",
-    password_confirmation: "password",
-    confirmation_token: "KwGQJGJDoePzr4u2AjWJ",
-    confirmed_at: "2018-08-02 05:51:48",
-    confirmation_sent_at: "2018-08-02 05:41:49"
-  },
-  {
-    email: "lachdoug@gmail.com",
-    password: "password",
-    password_confirmation: "password",
-    confirmation_token: "KxGQJGJDoePzr4u2AjWJ",
-    confirmation_sent_at: "2018-08-02 05:41:49"
-  },
+  # {
+  #   email: "lach@engines.org",
+  #   password: "password",
+  #   password_confirmation: "password",
+  #   confirmation_token: "KwGQJGJDoePzr4u2AjWJ",
+  #   confirmed_at: "2018-08-02 05:51:48",
+  #   confirmation_sent_at: "2018-08-02 05:41:49"
+  # },
+  # {
+  #   email: "lachdoug@gmail.com",
+  #   password: "password",
+  #   password_confirmation: "password",
+  #   confirmation_token: "KxGQJGJDoePzr4u2AjWJ",
+  #   confirmation_sent_at: "2018-08-02 05:41:49"
+  # },
   {
     email: "alfs@bigpond.com",
     password: "password",
