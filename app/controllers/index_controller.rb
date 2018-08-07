11c8fcb895eedd4c4d391fff3077957c2d249619
Lachlan Douglas <lachlan@engines.org>
2018-08-07 14:14:34 +1000

    render qestions with wrappers

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,9 +1,9 @@
 class IndexController < ApplicationController
 
-  def show
-    Rails.application.load_seed unless Admin.any?
-  rescue => e
-    render plain: "#{e}\n\n#{e.backtrace}"
-  end
+  # def show
+  #   Rails.application.load_seed unless Admin.any?
+  # rescue => e
+  #   render plain: "#{e}\n\n#{e.backtrace}"
+  # end
 
 end
