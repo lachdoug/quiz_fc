3340f17d00b8ba11efdf6e084147f92910651d07
Lachlan Douglas <lachlan@engines.org>
2018-10-07 14:13:54 +1100

    bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 load 'db/seeds/admins.rb'
-# if Rails.env == "development"
+if Rails.env == "development"
 #   load 'db/seeds/quizzes.rb'
 #   load 'db/seeds/questions.rb'
-#   load 'db/seeds/users.rb'
-# end
+  load 'db/seeds/users.rb'
+end
