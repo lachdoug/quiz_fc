28fe36ecb9d506477a7a91885361a2eb97172f9a
Lachlan Douglas <lachlan@engines.org>
2019-06-02 14:33:51 +1000

    actionmailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,19 +5,21 @@ Rails.application.configure do
   production_config = config_for :production
   action_mailer_config = production_config['action_mailer']
 
-  config.action_mailer.default_url_options = {
-    protocol: action_mailer_config['default_url_options']['protocol'],
-    host: action_mailer_config['default_url_options']['host']
-  }
+  config.action_mailer = action_mailer_config
 
-  config.action_mailer.delivery_method = action_mailer_config['delivery_method']
-
-  config.action_mailer.smtp_settings = {
-    address: action_mailer_config['smtp_settings']['address'],
-    port: action_mailer_config['smtp_settings']['port'],
-    domain: action_mailer_config['smtp_settings']['domain'],
-    enable_starttls_auto:  action_mailer_config['smtp_settings']['enable_starttls_auto'],
-  }
+  # config.action_mailer.default_url_options = {
+  #   protocol: action_mailer_config['default_url_options']['protocol'],
+  #   host: action_mailer_config['default_url_options']['host']
+  # }
+  #
+  # config.action_mailer.delivery_method = action_mailer_config['delivery_method']
+  #
+  # config.action_mailer.smtp_settings = {
+  #   address: action_mailer_config['smtp_settings']['address'],
+  #   port: action_mailer_config['smtp_settings']['port'],
+  #   domain: action_mailer_config['smtp_settings']['domain'],
+  #   enable_starttls_auto:  action_mailer_config['smtp_settings']['enable_starttls_auto'],
+  # }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -83,7 +85,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "quizfc_#{Rails.env}"
 
-  config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
